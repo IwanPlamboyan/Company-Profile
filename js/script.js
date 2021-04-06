9fdafc3ae3a2645868d97baa0db971a09be8d3ac
IwanPlamboyan <iwanplam03@gmail.com>
2021-04-05 21:48:15 -1200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  transition: 600,
+  interval: 3000,
+});
+
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+const materialboxed = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialboxed);
+
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+  scrollOffset: 50,
+});
